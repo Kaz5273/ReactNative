f68f8f7608508ad6e70c47acdbef08a761582673
kaz5273 <kazutor73@gmail.com>
2024-05-31 12:14:23 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -25,6 +25,29 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGZtWMqqG9CPDtbpKgf1giEIIXDCbRito",
+  authDomain: "quizz-320c0.firebaseapp.com",
+  projectId: "quizz-320c0",
+  storageBucket: "quizz-320c0.appspot.com",
+  messagingSenderId: "925635097878",
+  appId: "1:925635097878:web:7edf2969f6eb9c4ac1cc37",
+  measurementId: "G-MEB7R4MTLW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
